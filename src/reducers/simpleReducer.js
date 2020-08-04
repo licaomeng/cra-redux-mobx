26842ea5c01df9fcb6634178cd686aa18032b54d
Messi Li <licaomeng@gmail.com>
2020-08-04 15:20:47 +0800

    adjust sorting

--- src/reducers/simpleReducer.js
+++ src/reducers/simpleReducer.js
@@ -1,14 +1,14 @@
 const dataList = [
   { firstName: 'Messi', lastName: 'Li', gender: 'Male' },
+  { firstName: 'Cherie', lastName: 'Chung', gender: 'Female' },
   { firstName: 'Leslie', lastName: 'Cheung', gender: 'Male' },
   { firstName: 'Andy', lastName: 'Lau', gender: 'Male' },
+  { firstName: 'Sophie', lastName: 'Marceau', gender: 'Female' },
   { firstName: 'Jack', lastName: 'Cheung', gender: 'Male' },
   { firstName: 'Jackie', lastName: 'Chan', gender: 'Male' },
   { firstName: 'Alan', lastName: 'Tam', gender: 'Male' },
   { firstName: 'Scarlett', lastName: 'Johansson', gender: 'Female' },
-  { firstName: 'Sophie', lastName: 'Marceau', gender: 'Female' },
   { firstName: 'Marilyn', lastName: 'Monroe', gender: 'Female' },
-  { firstName: 'Cherie', lastName: 'Chung', gender: 'Female' },
   { firstName: 'Anita', lastName: 'Mui', gender: 'Female' }
 ]
 
